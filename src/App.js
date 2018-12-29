2f7c85eeb07c80c6719b621632a1c4a805498023
jorden <www.freewm@163.com>
2018-12-29 18:43:28 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Title from './components/Title';
+import Footer from './components/Footer';
+import AddTodo from './containers/AddTodo';
+import VisibleTodoList from './containers/visibleTodoList';
 
-import AddSession from './components/AddSession';
-import SearchSession from './components/SearchSession';
 
 class App extends Component {
     render() {
         return (
-            <div className="app">
-                <Title />
-                <AddSession />
-                <SearchSession />
+            <div>
+
+                <AddTodo />
+                <VisibleTodoList />
+                <Footer />
             </div>
         );
     }
--- src/components/Title.js
+++
@@ -1,5 +0,0 @@
-
-import React from 'react';
-
-
-export default  () => <div className="title">TODO LIST</div>;
